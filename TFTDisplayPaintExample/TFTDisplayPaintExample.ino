17d11b1118405088a93b00975e5fd48cf88e85ed
laynd <biz.mail.to@gmail.com>
2016-02-13 00:23:03 -0500

    progress with menu
    initial design of menu

--- TFTDisplayPaintExample/TFTDisplayPaintExample.ino
+++ TFTDisplayPaintExample/TFTDisplayPaintExample.ino
@@ -1,70 +1,32 @@
-// Paint example specifically for the TFTLCD breakout board.
-// If using the Arduino shield, use the tftpaint_shield.pde sketch instead!
-// DOES NOT CURRENTLY WORK ON ARDUINO LEONARDO
+/*
+Water pump controller V 0.1
+Built upon Adafruit TFT library examples
+Daniel Lameka 2/12/2016
 
-// Modified for SPFD5408 Library by Joao Lopes
-// Version 0.9.2 - Rotation for Mega
+*/
 
-// *** SPFD5408 change -- Begin
+
+
+
 #include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
 #include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
 #include <SPFD5408_TouchScreen.h>
-// *** SPFD5408 change -- End
 
 #if defined(__SAM3X8E__)
     #undef __FlashStringHelper::F(string_literal)
     #define F(string_literal) string_literal
 #endif
 
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-
-// For the Arduino Mega, use digital pins 22 through 29
-// (on the 2-row header at the end of the board).
-//   D0 connects to digital pin 22
-//   D1 connects to digital pin 23
-//   D2 connects to digital pin 24
-//   D3 connects to digital pin 25
-//   D4 connects to digital pin 26
-//   D5 connects to digital pin 27
-//   D6 connects to digital pin 28
-//   D7 connects to digital pin 29
-
-// For the Arduino Due, use digital pins 33 through 40
-// (on the 2-row header at the end of the board).
-//   D0 connects to digital pin 33
-//   D1 connects to digital pin 34
-//   D2 connects to digital pin 35
-//   D3 connects to digital pin 36
-//   D4 connects to digital pin 37
-//   D5 connects to digital pin 38
-//   D6 connects to digital pin 39
-//   D7 connects to digital pin 40
-
 #define YP A3  // must be an analog pin, use "An" notation!
 #define XM A2  // must be an analog pin, use "An" notation!
 #define YM 9   // can be a digital pin
 #define XP 8   // can be a digital pin
 
-// Original values
-//#define TS_MINX 150
-//#define TS_MINY 120
-//#define TS_MAXX 920
-//#define TS_MAXY 940
-
 // Calibrate values
-#define TS_MINX 125
-#define TS_MINY 85
-#define TS_MAXX 965
-#define TS_MAXY 905
+#define TS_MINX 115
+#define TS_MINY 115
+#define TS_MAXX 840
+#define TS_MAXY 840
 
 // For better pressure precision, we need to know the resistance
 // between X+ and X- Use any multimeter to read it
@@ -81,55 +43,34 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
 #define	BLUE    0x001F
-#define	RED     0xF800
-#define	GREEN   0x07E0
+#define	DARKPINK 0xD20B // my value
+#define	GREEN   0x2631 //my value
 #define CYAN    0x07FF
 #define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
+#define YELLOW  0xECE0 //my value
 #define WHITE   0xFFFF
+#define LIGHTGREY 0x2145 // myvalue
+#define DARKGREY 0x1083 //my value
 
+#define MINPRESSURE 5
+#define MAXPRESSURE 1000
+
+#define STATUSBAR 40
+#define MENUW 105
+#define MENUH 125
+#define BORDER 10
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
-#define BOXSIZE 40
-#define PENRADIUS 3
-int oldcolor, currentcolor;
+
+int menuSelect;
 
 void setup(void) {
   Serial.begin(9600);
   Serial.println(F("Paint!"));
   
   tft.reset();
-
-  // *** SPFD5408 change -- Begin
-//  uint16_t identifier = tft.readID();
-//
-//  if(identifier == 0x9325) {
-//    Serial.println(F("Found ILI9325 LCD driver"));
-//  } else if(identifier == 0x9328) {
-//    Serial.println(F("Found ILI9328 LCD driver"));
-//  } else if(identifier == 0x7575) {
-//    Serial.println(F("Found HX8347G LCD driver"));
-//  } else if(identifier == 0x9341) {
-//    Serial.println(F("Found ILI9341 LCD driver"));
-//  } else if(identifier == 0x8357) {
-//    Serial.println(F("Found HX8357D LCD driver"));
-//  } else {
-//    Serial.print(F("Unknown LCD driver chip: "));
-//    Serial.println(identifier, HEX);
-//    Serial.println(F("If using the Adafruit 2.8\" TFT Arduino shield, the line:"));
-//    Serial.println(F("  #define USE_ADAFRUIT_SHIELD_PINOUT"));
-//    Serial.println(F("should appear in the library header (Adafruit_TFT.h)."));
-//    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
-//    Serial.println(F("Also if using the breakout, double-check that all wiring"));
-//    Serial.println(F("matches the tutorial."));
-//    return;
-//  }
-//
-//  tft.begin(identifier);
-
   tft.begin(0x9341); // SDFP5408
-
   tft.setRotation(0); // Need for the Mega, please changed for your choice or rotation initial
 
   // Border
@@ -138,50 +79,30 @@ void setup(void) {
   
   // Initial screen
   
-  tft.setCursor (55, 50);
+  tft.setCursor (35, 85);
   tft.setTextSize (3);
-  tft.setTextColor(RED);
-  tft.println("SPFD5408");
-  tft.setCursor (65, 85);
-  tft.println("Library");
-  tft.setCursor (55, 150);
+  tft.setTextColor(YELLOW);
+  tft.println("Water Pump");
+  tft.setCursor (35, 120);
+  tft.println("Controller");
+  tft.setCursor (90, 160);
   tft.setTextSize (2);
-  tft.setTextColor(BLACK);
-  tft.println("TFT Paint");
-
-  tft.setCursor (80, 250);
+  tft.println("V 0.1");
+  tft.setCursor (75, 250);
   tft.setTextSize (1);
-  tft.setTextColor(BLACK);
+  tft.setTextColor(DARKPINK);
   tft.println("Touch to proceed");
 
   // Wait touch
-
   waitOneTouch();
+  tft.fillScreen(DARKGREY);
 
-// *** SPFD5408 change -- End
-
-  // -- End
-
-  // Paint
-  
-  tft.fillScreen(BLACK);
-
-  tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-  tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-  tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, GREEN);
-  tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, CYAN);
-  tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, BLUE);
-  tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-  // tft.fillRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, WHITE);
- 
-  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-  currentcolor = RED;
- 
+  statusBar();
+  drawMenu();
   pinMode(13, OUTPUT);
 }
 
-#define MINPRESSURE 10
-#define MAXPRESSURE 1000
+
 
 void loop()
 {
@@ -189,110 +110,114 @@ void loop()
   TSPoint p = ts.getPoint();
   digitalWrite(13, LOW);
 
-  // if sharing pins, you'll need to fix the directions of the touchscreen pins
   //pinMode(XP, OUTPUT);
   pinMode(XM, OUTPUT);
   pinMode(YP, OUTPUT);
   //pinMode(YM, OUTPUT);
 
-  // we have some minimum pressure we consider 'valid'
-  // pressure of 0 means no pressing!
-
   if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
-    /*
-    Serial.print("X = "); Serial.print(p.x);
-    Serial.print("\tY = "); Serial.print(p.y);
-    Serial.print("\tPressure = "); Serial.println(p.z);
-    */
-    
-    if (p.y < (TS_MINY-5)) {
-      Serial.println("erase");
-      // press the bottom of the screen to erase 
-      tft.fillRect(0, BOXSIZE, tft.width(), tft.height()-BOXSIZE, BLACK);
-    }
-    // scale from 0->1023 to tft.width
-
-    // *** SPFD5408 change -- Begin
-    // Bug in in original code
-    //p.x = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
     p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
-    // *** SPFD5408 change -- End
-    p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());;
-
-    /*
-    Serial.print("("); Serial.print(p.x);
-    Serial.print(", "); Serial.print(p.y);
-    Serial.println(")");
-    */
-    if (p.y < BOXSIZE) {
-       oldcolor = currentcolor;
-
-       if (p.x < BOXSIZE) { 
-         currentcolor = RED; 
-         tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*2) {
-         currentcolor = YELLOW;
-         tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*3) {
-         currentcolor = GREEN;
-         tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*4) {
-         currentcolor = CYAN;
-         tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*5) {
-         currentcolor = BLUE;
-         tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*6) {
-         currentcolor = MAGENTA;
-         tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, WHITE);
-       }
-
-       if (oldcolor != currentcolor) {
-          if (oldcolor == RED) tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-          if (oldcolor == YELLOW) tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-          if (oldcolor == GREEN) tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, GREEN);
-          if (oldcolor == CYAN) tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, CYAN);
-          if (oldcolor == BLUE) tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, BLUE);
-          if (oldcolor == MAGENTA) tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-       }
-    }
-    if (((p.y-PENRADIUS) > BOXSIZE) && ((p.y+PENRADIUS) < tft.height())) {
-      tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
-    }
+    p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+    
+    menuSelect = menuSelected(p.x, p.y);
   }
+  
+  
 }
 
 // Wait one touch
-
 TSPoint waitOneTouch() {
-
-  // wait 1 touch to exit function
-  
+ // wait 1 touch to exit function
   TSPoint p;
-  
   do {
     p= ts.getPoint(); 
-  
     pinMode(XM, OUTPUT); //Pins configures again for TFT control
     pinMode(YP, OUTPUT);
-  
   } while((p.z < MINPRESSURE )|| (p.z > MAXPRESSURE));
-  
   return p;
 }
 
 
-void drawBorder () {
+void drawBorder (){
+  tft.fillScreen(DARKGREY);
+  tft.fillRect(BORDER, BORDER, (tft.width() - BORDER * 2), (tft.height() - BORDER * 2), LIGHTGREY);
+}
 
-  // Draw a border
+void statusBar (){
+  tft.fillRect(0, 0, tft.width(), STATUSBAR, LIGHTGREY);
+  drawTime();
+}
 
-  uint16_t width = tft.width() - 1;
-  uint16_t height = tft.height() - 1;
-  uint8_t border = 10;
-
-  tft.fillScreen(RED);
-  tft.fillRect(border, border, (width - border * 2), (height - border * 2), WHITE);
+void drawMenu (){
+  tft.setTextSize (2);
+  tft.setTextColor(YELLOW);
+  
+  tft.fillRect(0, STATUSBAR, tft.width(), tft.height(), DARKGREY);
+  
+  tft.fillRect(BORDER, STATUSBAR+BORDER, MENUW, MENUH, LIGHTGREY);
+  tft.setCursor (BORDER+17, STATUSBAR+BORDER+55);
+  tft.println("Report");
+  tft.fillRect(MENUW+BORDER*2, STATUSBAR+BORDER, MENUW, MENUH, LIGHTGREY);
+  tft.setCursor (MENUW+BORDER*2+5, STATUSBAR+BORDER+55);
+  tft.println("Set Time");
+  tft.fillRect(BORDER, STATUSBAR+MENUH+BORDER*2, MENUW, MENUH, LIGHTGREY);
+  tft.setCursor (BORDER+30, STATUSBAR+MENUH+BORDER*2+40);
+  tft.println("Till");
+  tft.setCursor (BORDER+25, STATUSBAR+MENUH+BORDER*2+70);
+  tft.println("Alarm");
+  tft.fillRect(MENUW+BORDER*2, STATUSBAR+MENUH+BORDER*2, MENUW, MENUH, LIGHTGREY);
+  tft.setCursor (MENUW+BORDER*2+30, STATUSBAR+MENUH+BORDER*2+40);
+  tft.println("Till");
+  tft.setCursor (MENUW+BORDER*2+10, STATUSBAR+MENUH+BORDER*2+70);
+  tft.println("ShutOFF");
   
 }
 
+int menuSelected(int x, int y){
+  int a;
+  if(x > BORDER && x < BORDER+MENUW && y > STATUSBAR+BORDER && y < MENUH+BORDER+STATUSBAR) { a = 1; }
+  if(x > MENUW+BORDER*2 && x < MENUW*2+BORDER*2 && y > STATUSBAR+BORDER && y < MENUH+BORDER+STATUSBAR) { a = 2; }
+  if(x > BORDER && x < BORDER+MENUW && y > MENUH+STATUSBAR+BORDER*2 && y < MENUH*2+STATUSBAR+BORDER*2) { a = 3; }
+  if(x > MENUW+BORDER*2 && x < MENUW*2+BORDER*2 && y > MENUH+STATUSBAR+BORDER*2 && y < MENUH*2+STATUSBAR+BORDER*2) { a = 4; }
+  Serial.print("A: ");
+  Serial.println(a);
+  
+  return a;
+}
 
+
+void drawTime(){
+    
+    String timeString = "10:33";
+    String lastTimeON = "09:16";
+    int duration = 34;
+    int galUsed = 14;
+    
+    tft.setTextSize (2);
+    tft.setTextColor(GREEN);
+    tft.setCursor(170, 13);
+    tft.println(timeString);
+    
+    tft.setTextSize (1);
+    tft.setTextColor(DARKPINK);
+    tft.setCursor(10, 6);
+    tft.println("Last time ON:");
+    tft.setCursor(10, 16);
+    tft.println("Duration:");
+    tft.setCursor(85, 16);
+    tft.println("minutes");
+    tft.setCursor(10, 26);
+    tft.println("Gallons used:");
+    
+    tft.setTextColor(GREEN);
+    tft.setCursor(90, 6);
+    tft.println(lastTimeON);
+    tft.setCursor(65, 16);
+    tft.println(duration);
+    tft.setCursor(88, 26);
+    tft.println(galUsed);
+}
+
+
+  
+  
